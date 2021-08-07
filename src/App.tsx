afc47833285d7aec8428a545e214a3e4d6a1cb94
yongjoon <penubokim@gmail.com>
2021-08-07 11:55:34 +0900

    Add Entry React component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  return (
+    <div className="App">
+        hello world
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from 'App.tsx';
 
-const App = () => {
-    return (
-        <h1>
-            Hello World
-        </h1>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
+);
